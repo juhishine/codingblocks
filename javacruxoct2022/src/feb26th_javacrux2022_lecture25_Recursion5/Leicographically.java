1c35d78583c9e94ccbea194a43e9e12ad55a7112
Juhi <juhishine1@gmail.com>
2023-02-26 13:21:43 +0530

    first code

--- javacruxoct2022/src/feb26th_javacrux2022_lecture25_Recursion5/Leicographically.java
+++ javacruxoct2022/src/feb26th_javacrux2022_lecture25_Recursion5/Leicographically.java
@@ -1,6 +1,17 @@
 package feb26th_javacrux2022_lecture25_Recursion5;
 
 public class Leicographically {
+	/*
+	 0    1 10 100 1000 101 102......... 109
+	 11   110 111 112..................119
+	 12   120 121 122...................129
+	 ..
+	 ...
+	 19 190 191 .......................199
+	 19 190 1900 
+	 2 20 200 201 202....................209
+	 9 99 999 9999
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
