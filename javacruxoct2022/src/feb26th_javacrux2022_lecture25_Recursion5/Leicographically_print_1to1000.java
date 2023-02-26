c5eee3d08be4bfa72bd2371df44f7593008ca427
Juhi <juhishine1@gmail.com>
2023-02-26 13:37:56 +0530

    commit1

--- javacruxoct2022/src/feb26th_javacrux2022_lecture25_Recursion5/Leicographically_print_1to1000.java
+++ javacruxoct2022/src/feb26th_javacrux2022_lecture25_Recursion5/Leicographically_print_1to1000.java
@@ -1,6 +1,6 @@
 package feb26th_javacrux2022_lecture25_Recursion5;
 
-public class Leicographically {
+public class Leicographically_print_1to1000 {
 	/*
 	 0    1 10 100 1000 101 102......... 109
 	 11   110 111 112..................119
@@ -11,10 +11,27 @@ public class Leicographically {
 	 19 190 1900 
 	 2 20 200 201 202....................209
 	 9 99 999 9999
+	 less than 1000
 	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int n=1000;
+		counting(0,n);
+	}
+	public static void counting(int curr,int end)
+	{
+		if(curr>end)
+			{return;
+			}
+		System.out.println(curr);
+		int i = 0;
+		if(curr==0)
+			{i=1;}
+		for (; i <=9; i++) {
+			counting(curr*10+i,end);
+		}
+
 
 	}
 
